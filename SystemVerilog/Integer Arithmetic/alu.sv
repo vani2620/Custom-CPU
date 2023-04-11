bdea433808f9227d3add731c61128e44ef69d61d
vani2620 <3idiotsrule@gmail.com>
2023-04-11 02:43:35 -0400

    ALU ops added; still incomplete

--- SystemVerilog/Integer Arithmetic/alu.sv
+++ SystemVerilog/Integer Arithmetic/alu.sv
@@ -7,14 +7,25 @@ module alu #(
 
     input [SHIFT_AMT - 1:0] shift_code,
 
-    input invert_a,
-    input invert_b,
-    input invert_out,
-    input or_enable,
-    input carry_in,
+    input [4:0] func_code,
 
-    output [DATA_WIDTH - 1:0] data_out,
-    output carry_out
+    output [DATA_WIDTH - 1:0] data_out
 );
-
+// verilog_lint: waive-start line-length
+always_comb begin
+    case (func_code)
+        5'b00000: result = $unsigned(data_in_a) + $unsigned(data_in_b); //ADD unsigned
+        5'b00001: result = $signed(data_in_a) + $signed(data_in_b); //ADD Signed
+        5'b00010: result = $unsigned(data_in_a) - $unsigned(data_in_b); //SUB
+        5'b00011: result = $signed(data_in_a) - $signed(data_in_b); //SUB Signed
+        5'b00100: result = data_in_a ^ data_in_b; //bitwise XOR
+        5'b00101: result = data_in_a & data_in_b; //bitwise AND
+        5'b00110: result = data_in_a | data_in_b; //bitwise OR
+        5'b00111: result = ($unsigned(data_in_a) < $unsigned(data_in_b)) ? 64'h1 : 64'h0; //Set Less Than
+        5'b01000: result = ($signed(data_in_a) < $signed(data_in_b)) ? 64'h1: 64'h0;
+        5'b01001: result = ~data_in_a + 1;
+        default: data_out = 64'h0;
+    endcase
+end
+// verilog_lint: waive-stop line-length
 endmodule
