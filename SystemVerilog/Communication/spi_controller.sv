82556166699e813c421c0ce7cbd6ee7523f9ce67
vani2620 <3idiotsrule@gmail.com>
2023-04-12 19:24:03 -0400

    progress update 120420231924

--- SystemVerilog/Communication/spi_controller.sv
+++ SystemVerilog/Communication/spi_controller.sv
@@ -5,33 +5,59 @@ module spi_controller #(
     input clk,
     input clk_en,
     input sync_rst_n,
+    input start_txn,
 
     input wr_en,
 
     input [1:0] spi_mode,
     input [2:0] byte_sel, //enables the transmission of one to eight bytes
-    input start_txn,
+
     input [DATA_WIDTH - 1:0] parallel_wr_data,
 
-    input [PERI_CNT - 1:0] peri_select_in,
+    input [PERI_CNT - 1:0] chip_sel_one_cold,
 
     input poci, //peripheral out, controller in
 
     output copi, //controller out, peripheral in
-    output [PERI_CNT - 1:0] peri_select_out
+    output [PERI_CNT - 1:0] s_chip_sel_one_cold,
+    output count,
+    output end_txn
 );
 
 reg [DATA_WIDTH - 1:0] data;
 reg [$clog2(DATA_WIDTH) - 1:0] counter;
+reg [] spi_csr;
+// Register fields
+    //spi_csr[1:0] = spi_mode
+        //spi_mode[0] = CPOL
+        //spi_mode[1] = CPHA
+    //spi_csr[4:2] = byte_sel
+    //spi_csr[8:5] = state[3:0] (TODO)
+        //state[0] = valid?
+        //state[1] = txn in progress?
+        //state[2] = idle/done
 
-always_ff @(posedge clk) begin
+//RESET
+always_ff @(posedge clk or negedge sync_rst_n) begin
     if (!sync_rst_n) begin
         data <= 0;
+        counter <= 0;
+        copi <= 0;
     end
-    else if (clk_en && wr_en) begin
+end
+
+
+always_ff @(posedge clk) begin
+    //Write data from core
+    if (clk_en && wr_en) begin
         data <= parallel_wr_data;
     end
-    else if (clk_en && comm_en) begin
+    //Transaction
+    else if (clk_en && start_txn) begin
+        counter <= counter - 1;
+        count <= counter;
+        data <= {din, data[6:0]};
+        pico <= data[7];
     end
 end
 
--- SystemVerilog/Control/gen_map_table.sv
+++ SystemVerilog/Control/gen_map_table.sv
@@ -4,14 +4,24 @@ module gen_map_table #(
     parameter int VIRT_COUNT = 256,
     parameter int VIRT_ADDR_WIDTH = $clog2(VIRT_COUNT),
     parameter int WRITE_PORTS = 4,
-    parameter int READ_PORTS = 4,
+    parameter int READ_PORTS = 8,
     parameter int PHYS_ADDR_WIDTH = $clog2(CELLS)
 )(
     input clk,
     input clk_en,
     input sync_rst_n,
 
-    input [ARCH_ADDR_WIDTH - 1:0] arch_addr [WRITE_PORTS]
+    input [WRITE_PORTS - 1:0] wr_en,
+    input [ARCH_ADDR_WIDTH - 1:0] wr_tbl_addr [WRITE_PORTS],
+
+    input [PHYS_ADDR_WIDTH - 1:0] wr_arch_addr [WRITE_PORTS],
+    input [VIRT_ADDR_WIDTH - 1:0] wr_virt_addr [WRITE_PORTS],
+    input valid,
+
+    input [READ_PORTS - 1:0] rd_en,
+
+    output [PHYS_ADDR_WIDTH - 1:0] rd_arch_addr [READ_PORTS],
+    output [VIRT_ADDR_WIDTH - 1:0] rd_virt_addr [READ_PORTS]
 );
 
 endmodule
