25dba6951eaaf3e814c04a3ebf65e7f4ad71289a
vani2620 <3idiotsrule@gmail.com>
2023-10-25 01:06:57 -0400

    update 202310250107 - Rewrote program_counter.sv TODO: - instruction decoder - instruction memory - data memory - memory controller - other things I can't recall

--- SystemVerilog/Branching/program_counter.sv
+++ SystemVerilog/Branching/program_counter.sv
@@ -4,8 +4,6 @@
 // verilog_lint: waive-start parameter-name-style
 
 module program_counter #(
-    parameter int PC_SHORT_OFFSET_WIDTH = 12,
-    parameter int PC_LONG_OFFSET_WIDTH = 20,
     parameter int INSTR_ADDR_WIDTH = 32,
     parameter int REG_ADDR_WIDTH = 5
 )(
@@ -16,54 +14,51 @@ module program_counter #(
     input wire stall,
 
     input wire branch_en,
-    input wire jump_link_en,
-    input wire long_offset_en,
+    input wire jump_en,
+    input wire link_en,
+    input wire [4:0] branch_cond,
 
-    input wire [REG_ADDR_WIDTH - 1 : 0] link_addr_in,
-    input wire [PC_SHORT_OFFSET_WIDTH - 1 : 0] short_offset,
-    input wire [PC_LONG_OFFSET_WIDTH - 1 : 0] long_offset,
+    input wire [INSTR_ADDR_WIDTH - 1:0] branch_base_addr,
 
-    output reg [INSTR_ADDR_WIDTH - 1 : 0] next_instr_addr,
-    output reg [REG_ADDR_WIDTH - 1 : 0] link_addr_out
+    input wire [INSTR_ADDR_WIDTH - 1:0] short_offset, //16-bit signed offset, sign-extended to 32 bits, used with register-relative addressing
+    input wire [INSTR_ADDR_WIDTH - 1:0] long_offset, //24-bit signed offset, sign-extended to 32 bits, used with PC-relative addressing
+
+    output reg [INSTR_ADDR_WIDTH - 1:0] next_instr_addr,
+    output reg [REG_ADDR_WIDTH - 1:0] link_reg_addr
 );
 
-reg [INTSR_ADDR_WIDTH - 1 : 0] cur_instr_reg;
-reg [REG_ADDR_WIDTH - 1 : 0] link_addr_reg;
+reg [INSTR_ADDR_WIDTH - 1:0] counter;
+reg [4:0] conditions;
 
-//Program counter logic
-always_ff @(posedge clk) begin
-    if (!sync_rst_n) begin
-        cur_instr_reg <= 0;
-        link_addr_reg <= 0;
-    end
-    else if (clk_en) begin
-        //If stall is asserted, do not increment the program counter
-        while (stall) begin
-            next_instr_addr <= cur_instr_reg;
-        end
-        //If branch is asserted, add the branch offset to the program counter
-        if (branch_en) begin
-            next_instr_addr <= cur_instr_reg + (short_offset << 1);
-        end
-        //If relative jump is asserted, add the jump offset to the program counter
-        else if (jump_link_en) begin
-            //If link is asserted, store the current program counter in the link register
-            if (link_en) begin
-                link_addr_reg <= link_addr_in;
-            end
-            else begin
-                link_addr_reg <= cur_instr_reg + 4;
-            end
-            next_instr_addr <= cur_instr_reg + (short_offset << 1);
-        end
-        else if (long_offset_en) begin
-            next_instr_addr <= cur_instr_reg + (long_offset << 12);
-        end
-        else begin
-            next_instr_addr <= cur_instr_reg + 4;
-        end
+wire [INSTR_ADDR_WIDTH - 1:0] inc_counter = counter << 1; //byte-addressed memory; shortest instructions are 2 bytes
+
+wire [INSTR_ADDR_WIDTH - 1:0] temp_next_instr_addr;
+
+always_comb begin : calcNext
+    if (stall) begin
+        temp_next_instr_addr = counter;
+    end else if (branch_en) begin
+        temp_next_instr_addr = branch_base_addr + (short_offset << 1);
+    end else if (jump_en) begin
+        temp_next_instr_addr = counter + (long_offset << 1);
+    end else begin
+        temp_next_instr_addr = inc_counter;
     end
 end
+
+always_comb begin : linkReg
+    link_reg_addr = inc_counter;
+end
+
+always_ff @(posedge clk) begin : updatePC
+    if (!sync_rst_n) begin
+        counter <= 0;
+        conditions <= 0;
+    end else if (clk_en) begin
+        counter <= temp_next_instr_addr;
+    end
+end
+
 
 endmodule
 
